66d9aeef24b3f98bec63342200a9c9dbc5d99df4
Porter Shawver <portershawver@gmail.com>
2023-07-09 10:48:42 -0500

    feat(Category): create the category class

---
+++ lib/types/Category.dart
@@ -0,0 +1,11 @@
+/// This file contains the definition of the [Category] type.
+
+/// Public class [Category] simply represents a category created by the user.
+/// It exists to make it easier to add new features in the future.
+class Category {
+  /// The name of the category.
+  final String name;
+
+  /// Creates a new [Category] with the given [name].
+  const Category({required this.name});
+}
