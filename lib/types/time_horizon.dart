34792025e5ff2c6871b8c9b066d055f7529bd443
Porter Shawver <portershawver@gmail.com>
2023-07-12 18:31:30 -0500

    feat(TimeHorizon filter): change default button order

--- lib/types/time_horizon.dart
+++ lib/types/time_horizon.dart
@@ -5,10 +5,10 @@ import 'dropdown_menu_item.dart';
 /// Enum representing different lengths of time. To be used for filtering tasks
 /// based on when they are due.
 enum TimeHorizon implements FilterMenuItem<TimeHorizon> {
+  all(string: 'All'),
   day(string: 'Day'),
   week(string: 'Week'),
-  month(string: 'Month'),
-  all(string: 'All');
+  month(string: 'Month');
 
   const TimeHorizon({required this.string});
 
