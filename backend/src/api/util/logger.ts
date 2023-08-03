1c681703ad298beac21147a28da1d2c862f1cb72
Porter Shawver <portershawver@gmail.com>
2023-08-02 21:11:34 -0500

    feat(b: logger): add color

--- backend/src/api/util/logger.ts
+++ backend/src/api/util/logger.ts
@@ -1,4 +1,31 @@
 import winston from 'winston';
+import Transport from 'winston-transport';
+
+enum Colors {
+    error = "\x1b[31m",
+    warn = "\x1b[33m",
+    info = "\x1b[32m",
+    http = "\x1b[36m",
+    debug = "\x1b[34m",
+    silly = "\x1b[35m",
+};
+
+class ColorConsoleTrasport extends Transport {
+    log(info: winston.Logform.TransformableInfo, callback: () => void) {
+        setImmediate(() => {
+            this.emit('logged', info);
+        });
+
+        const color = Colors[info.level as keyof typeof Colors];
+        if (color) {
+            console.log(color, info.message);
+        } else {
+            console.log(info.message);
+        }
+
+        callback();
+    }
+}
 
 // Logger format
 const format = winston.format.printf(
@@ -7,12 +34,12 @@ const format = winston.format.printf(
     });
 
 const logger = winston.createLogger({
-    level: 'info',
+    level: 'debug',
     format: winston.format.combine(
         winston.format.timestamp({ format: 'HH:mm:ss' }),
         format
     ),
-    transports: [new winston.transports.Console()],
+    transports: [new ColorConsoleTrasport()],
 });
 
 export default logger;
